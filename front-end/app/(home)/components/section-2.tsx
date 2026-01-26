0fefe88fc5954d93356129783631838d5900b6f3
renan22rsw <renan22rsw@gmail.com>
2026-01-26 17:18:39 -0300

    feat:section 2 (need to change the text and images)

---
+++ front-end/app/(home)/components/section-2.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+import UnicornParty from "@/public/unicorn-party.png";
+import SunFlowerParty from "@/public/sunflower-party.png";
+import BithdayParty from "@/public/birthday-party.png";
+
+const prodcuts = [
+  {
+    id: 1,
+    name: "Unicorn Party",
+    image: UnicornParty,
+  },
+  {
+    id: 2,
+    name: "Sunflower Party",
+    image: SunFlowerParty,
+  },
+  {
+    id: 3,
+    name: "Bithday Party",
+    image: BithdayParty,
+  },
+];
+
+const Section2 = () => {
+  return (
+    <section className="bg-[#F4F3F1]">
+      <div className="space-y-4 pt-32 text-center">
+        <span className="text-lg font-semibold text-[#E85555]">
+          Nossos Serviços
+        </span>
+        <h1 className="text-5xl font-bold">Tudo para sua Festa</h1>
+        <p className="text-secondary-foreground/60 px-6 text-2xl font-medium">
+          Oferecemos solucoes completas em decoracao para todos os tipos de{" "}
+          eventos e celebracoes
+        </p>
+      </div>
+
+      <div className="mx-auto grid gap-8 px-4 py-8 md:grid-cols-2 lg:w-360 lg:grid-cols-3">
+        {prodcuts.map((prodcuts) => (
+          <div
+            key={prodcuts.id}
+            className="relative aspect-4/3 overflow-hidden"
+          >
+            <Image
+              src={prodcuts.image}
+              alt={prodcuts.name}
+              fill
+              className="rounded-2xl object-cover transition-all duration-150 hover:shadow-2xl"
+              priority
+            />
+
+            <div className="from-foreground/80 absolute bottom-0 flex h-full w-full flex-col justify-end gap-2 rounded-2xl bg-linear-to-t to-transparent p-4">
+              <h3 className="text-primary-foreground text-2xl font-bold">
+                Teste
+              </h3>
+              <p className="text-primary-foreground text-md">Descrição teste</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Section2;
--- front-end/app/(home)/page.tsx
+++ front-end/app/(home)/page.tsx
@@ -1,9 +1,11 @@
-import { Section1 } from "./components/section-1";
+import Section1 from "./components/section-1";
+import Section2 from "./components/section-2";
 
 const HomePage = () => {
   return (
     <>
       <Section1 />
+      <Section2 />
     </>
   );
 };
